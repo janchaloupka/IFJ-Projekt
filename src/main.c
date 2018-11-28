ba4eeccdfcb56078ac97a4f215a85affa41add39
Vit Sebela <sebela.vit@gmail.com>
2018-11-28 23:16:42 +0100

    Update mainu - makro a odsazeni

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define NUMERO_OF_TESTS 11
+
 int main(int argc, char const *argv[])
 {
 
@@ -15,57 +17,56 @@ int main(int argc, char const *argv[])
 }
 int vitaDebug(){
 
-	FILE *file_test[11]; // pocet testu -> lepsi udelat makro
-  	FILE *file_expected[11];
+	FILE *file_test[NUMERO_OF_TESTS]; // pocet testu -> lepsi udelat makro
+	FILE *file_expected[NUMERO_OF_TESTS];
 
-  	char c = ' ';
-  	char test[30];
-    char testOut[30];
-    pToken token = NULL;
+	char c = ' ';
+	char test[30];
+  char testOut[30];
+  pToken token = NULL;
 
-  	for (int i = 0; i < 11; i++)
-  	{
-  		printf("\033[1;33m");
-	    printf("------------------- TEST_%d -------------------|\n", i+1);
-	    printf("\033[0m");
-	    sprintf(test, "tests/tests_corr/test%d", i+1);
-	    sprintf(testOut, "tests/tests_corr/test%d.out", i+1);
+	for (int i = 0; i < NUMERO_OF_TESTS; i++)
+	{
+			printf("\033[1;33m");
+    	printf("------------------- TEST_%d -------------------|\n", i+1);
+    	printf("\033[0m");
+    	sprintf(test, "tests/tests_corr/test%d", i+1);
+    	sprintf(testOut, "tests/tests_corr/test%d.out", i+1);
 
-	    if ((file_expected[i] = fopen(testOut, "r")) == NULL){
-	    	printf("nepodarilo se otevrit soubor %s", testOut);
-	    	return -1;
-	    }
-	    if ((file_test[i] = fopen(test, "r")) == NULL){
-	    	printf("nepodarilo se otevrit soubor %s", test);
-	    	return -1;
-	    }
-	    
-	    c = ' ';
-	    while (c != EOF) {
+		  if ((file_expected[i] = fopen(testOut, "r")) == NULL){
+		  		printf("nepodarilo se otevrit soubor %s", testOut);
+		  		return -1;
+		  }
+		  if ((file_test[i] = fopen(test, "r")) == NULL){
+		  	printf("nepodarilo se otevrit soubor %s", test);
+		  	return -1;
+		  }
+
+		  c = ' ';
+		  while (c != EOF) {
 		    c = getc(file_expected[i]);
 		    printf("%c",c);
-	    }
-	    printf("\n\n");
+		  }
+		  printf("\n\n");
 
-        scannerGetTokenList(&token, file_test[i]);
-        parser(&token);
-        fclose(file_test[i]);
-        fclose(file_expected[i]);
-		scannerFreeTokenList(&token);
-		_scannerFSM(NULL, NULL);
-		printf("\033[1;31m");
-      	printf("\n________________END OF TEST_%d_________________|\n", i+1);
-      	printf("\033[0m");
-      	if (i < 10) printf("|\n|\n");
-
-  	}
+		  scannerGetTokenList(&token, file_test[i]);
+		  parser(&token);
+		  fclose(file_test[i]);
+		  fclose(file_expected[i]);
+			scannerFreeTokenList(&token);
+			_scannerFSM(NULL, NULL);
+			printf("\033[1;31m");
+			printf("\n________________END OF TEST_%d_________________|\n", i+1);
+			printf("\033[0m");
+			if (i < NUMERO_OF_TESTS-1) printf("|\n|\n");
+	}
   return 0;
 }
 int yellDebug(){
 	FILE *source1 = fopen("tests/test-code.1", "r");
 	FILE *source2 = fopen("tests/test-code.2", "r");
 	FILE *source3 = fopen("tests/test-code.3", "r");
-	
+
 	pToken token1 = NULL;
 	pToken token2 = NULL;
 	pToken token3 = NULL;
@@ -92,7 +93,7 @@ int yellDebug(){
 
 int janchDebug(){
 	FILE *source = fopen("tests/test-expr", "r");
-	
+
 	psTree localId;
 	symTabInit(&localId);
 	psData data = malloc(sizeof(struct sData));
@@ -106,7 +107,7 @@ int janchDebug(){
 	parser(&token);
 	//exprParse(&token, localId);
 	printf("\nPOPS GF@$tmp\nWRITE GF@$tmp\nWRITE string@\\010\n");
-	
+
 	fclose(source);
 	scannerFreeTokenList(&token);
 	symTabDispose(&localId);
